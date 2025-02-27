89a71b5746f55fc9f516df48cf038825a6bce18a
xDevagya <devagya.007@gmail.com>
2025-02-27 17:39:22 +0530

    Changes for LLM

--- backend/app/routes/llm.py
+++ backend/app/routes/llm.py
@@ -1,10 +1,32 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+
+chat_history = []
+
+async def startNewChat():
+    chat_history = []
+    return True
+
 class LLMRequest(BaseModel):
     query:str
 
 router = APIRouter()
 @router.post("/chat")
 async def chat(request: LLMRequest):
-    return f"llm.py():/chat: request:{request}"
+    chat_history.append(HumanMessage(request.query))
+    ai_response = llm.invoke(chat_history).content
+    chat_history.append(AIMessage(ai_response))
+
+    return ai_response
+
+@router.get("/chat")
+async def get_chat_history():
+    return chat_history
